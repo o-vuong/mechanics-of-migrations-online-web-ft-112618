0ea7d57a5aceac6edb4483b8976fcead79e9d8bc
Oscar Vuong <oscarvuong@gmail.com>
2019-01-14 03:39:22 +0000

    Automatically backed up by Learn

---
+++ config/environment.rb
@@ -0,0 +1,15 @@
+require 'rake'
+require 'active_record'
+require 'yaml/store'
+require 'ostruct'
+require 'date'
+
+
+require 'bundler/setup'
+Bundler.require
+
+
+# put the code to connect to the database here
+
+
+require_relative "../artist.rb"
